e3606cb22a4378559f040e630083f79a75768d93
Ulrich <ulrich@rightcom.com>
2025-11-19 18:39:55 +0100

    day_3

---
+++ day_3.py
@@ -0,0 +1,12 @@
+hasArmor = True
+powerLevel = 85
+
+if hasArmor and powerLevel > 80 :
+    print("Your character is a variation of Iron Man!");
+
+elif hasArmor and powerLevel > 80: 
+    print("Your character looks more like Thor!")
+
+else :
+   print("You are an intermediate-level hero.")
+
